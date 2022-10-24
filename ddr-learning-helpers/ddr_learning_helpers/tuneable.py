70224baae13f6e8fd41b62f9177e1029da31c75c
Wagner Almeida <69909128+wagneraljr@users.noreply.github.com>
2022-10-24 16:44:43 -0300

    Update tuneable.py

--- ddr-learning-helpers/ddr_learning_helpers/tuneable.py
+++ ddr-learning-helpers/ddr_learning_helpers/tuneable.py
@@ -10,7 +10,7 @@ warnings.simplefilter("ignore")
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 import tensorflow as tf
-
+if type(tf.contrib) != type(tf): tf.contrib._warning = None
 tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
 
 from .runs import *
