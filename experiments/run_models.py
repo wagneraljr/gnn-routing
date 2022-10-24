4307959f49bbcce1251519be38ac704ff8541b54
Wagner Almeida <69909128+wagneraljr@users.noreply.github.com>
2022-10-24 16:45:18 -0300

    Update run_models.py

--- experiments/run_models.py
+++ experiments/run_models.py
@@ -3,7 +3,7 @@ import os
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 import tensorflow as tf
-
+if type(tf.contrib) != type(tf): tf.contrib._warning = None
 tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
 
 import gym
